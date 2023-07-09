6aaad23b0371e7cc4c8798a7b6e296aef5d74cd8
AlexsandarB <110353772+AlexsandarB@users.noreply.github.com>
2023-07-09 17:57:38 +0200

    Update queries.sh

--- queries.sh
+++ queries.sh
@@ -16,21 +16,17 @@ echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
 echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
 echo "$($PSQL "SELECT ROUND(AVG(winner_goals),2) FROM games")"
 
-
 echo -e "\nAverage number of goals in all games from both teams:"
 echo "$($PSQL "SELECT (AVG(winner_goals)+AVG(opponent_goals)) FROM games")"
 
-
 echo -e "\nMost goals scored in a single game by one team:"
 echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
-
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
 echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "SELECT name FROM games LEFT JOIN teams ON games.winner_id=teams.team_id WHERE year = 2018 AND round='Final'")"
-
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
 echo "$($PSQL "SELECT DISTINCT(name) FROM teams LEFT JOIN games ON teams.team_id=games.winner_id or teams.team_id=games.opponent_id WHERE year = 2014 ORDER BY name")"
@@ -42,4 +38,4 @@ echo -e "\nYear and team name of all the champions:"
 echo "$($PSQL "SELECT year, name FROM games LEFT JOIN teams ON games.winner_id=teams.team_id WHERE round='Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%' ORDER BY name")"
